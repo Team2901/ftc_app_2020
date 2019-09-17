fc03ee5b9ecf62e0a9129cc888b8a365a103dd29
Nicole C Allen <allenn23825@student.cghsnc.org>
2019-09-17 17:03:38 -0400

    Update Swerve Test Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SwerveTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SwerveTestTeleOp.java
@@ -1,25 +1,58 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp (name = "swerve test teleop")
 public class SwerveTestTeleOp extends OpMode {
-    public final static double WHEEL_SERVO_GEAR_RATIO = 24.0/80.0;
-    public final static int SERVO_MAX_ANGLE = 180;
+    public final static double WHEEL_SERVO_GEAR_RATIO = 1/1;
+    public final static int SERVO_MAX_ANGLE = 190;
+    Servo servoFrontLeft;
     @Override
     public void init() {
-
+        servoFrontLeft = hardwareMap.servo.get("servoFrontLeft");
     }
 
     @Override
     public void loop() {
+        double joystickPositionX = gamepad1.left_stick_x;
+        double joystickPositionY = -gamepad1.left_stick_y;
 
+        double wheelAngle = joystickPositionToWheelAngle(joystickPositionX,joystickPositionY);
+        double servoAngle = wheelServoAngle(wheelAngle);
+        double servoPosition = servoAngleToPosition(servoAngle);
+        servoFrontLeft.setPosition(servoPosition);
+
+        telemetry.addData("wheelAngle",wheelAngle);
+        telemetry.addData("servoAngle",servoAngle);
+        telemetry.addData("servoPosition",servoPosition);
+        telemetry.addData("actual servo position",servoFrontLeft.getPosition());
+        telemetry.update();
     }
+
     public double wheelServoAngle(double wheelAngle){
         double servoAngle = WHEEL_SERVO_GEAR_RATIO * wheelAngle;
         return servoAngle;
     }
+
     public double servoAngleToPosition(double servoAngle){
        double servoPosition = servoAngle/SERVO_MAX_ANGLE;
        return servoPosition;
     }
+
+    public double joystickPositionToWheelAngle (double joystickPositionX, double joystickPositionY){
+        double wheelAngle = Math.atan2(joystickPositionY,joystickPositionX);
+        return wheelAngle;
+    }
+
+
+
+
+
+
+
+
+
+
 }
