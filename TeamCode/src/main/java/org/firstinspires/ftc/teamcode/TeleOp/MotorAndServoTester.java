1334638d8330c5d21f7bf6b67fd518e4d90ee5d4
Margaret Toebes <mtoebes@republicwireless.com>
2019-09-23 13:21:24 +0000

    Create MotorAndServoTester.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MotorAndServoTester.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "MotorAndServoTester")
+public class MotorAndServoTester extends OpMode {
+    Servo mrServo;
+    Servo mrServo2;
+
+    DcMotor motor;
+
+    @Override
+    public void init() {
+        mrServo = hardwareMap.servo.get("servo");
+        mrServo2 = hardwareMap.servo.get("servo_2");
+        motor = this.hardwareMap.dcMotor.get("motor");
+
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mrServo.setPosition(0);
+        mrServo2.setPosition(0);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.x){
+            mrServo.setPosition(0);
+        }
+        if (gamepad1.a){
+            mrServo.setPosition(1.0/3);
+        }
+        if (gamepad1.b){
+            mrServo.setPosition(2.0/3);
+        }
+        if (gamepad1.y){
+            mrServo.setPosition(1.0);
+
+        }
+
+        float leftStickY = -gamepad1.left_stick_y;
+
+        motor.setPower(leftStickY);
+
+        telemetry.addData("Servo position",mrServo.getPosition());
+        telemetry.addData("Motor power",motor.getPower());
+        telemetry.addData("Motor ticks",motor.getCurrentPosition());
+
+        telemetry.addData("Press X","goes to position 0");
+        telemetry.addData("Press A","goes to position 0.33");
+        telemetry.addData("Press B","goes to position 0.66");
+        telemetry.addData("Press Y","goes to position 1.0");
+        telemetry.addData("Left Joystick Y","sets motor power");
+        telemetry.update();
+
+    }
+}
