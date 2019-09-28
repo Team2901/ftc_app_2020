f8dfdd76e210768a2dada2f16d118329e7a5b356
BrigitteGallagher <32017661+BrigitteGallagher@users.noreply.github.com>
2019-09-28 13:47:07 -0400

    programming mechanum robot program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseSampleCode/Programmer_MechanumHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseSampleCode/Programmer_MechanumHardware.java
@@ -1,17 +1,13 @@
 package org.firstinspires.ftc.teamcode.BaseSampleCode;
 
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.DcMotorSimple;
-        import com.qualcomm.robotcore.hardware.HardwareMap;
-        import com.qualcomm.robotcore.hardware.Servo;
-/**
- * Created by piscullin18641 on 1/30/2018.
- */
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
-/*
-PSA DXM stands for Deus Ex Machina
- */
-public class MechanumHardware {
+public class Programmer_MechanumHardware {
+
+
 /*
 I created this naming system as the letter f or b meaning front or back because
 with machanum wheels all 4 wheels need to be motorized
@@ -21,15 +17,17 @@ with machanum wheels all 4 wheels need to be motorized
     public DcMotor fRight = null;
     public DcMotor bLeft = null;
     public DcMotor bRight = null;
-    public DcMotor lift = null;
+   // public DcMotor lift = null;
+    public DcMotor arm = null;
 
-
-
+/*
     public Servo jewelKnockDevice = null;
     public Servo bLeftPincer = null;
     public Servo bRightPincer = null;
     public Servo tLeftPincer = null;
     public Servo tRightPincer = null;
+    */
+    public Servo clawServo = null;
 
     HardwareMap hwMap = null;
 
@@ -41,39 +39,44 @@ with machanum wheels all 4 wheels need to be motorized
         fRight = hwMap.dcMotor.get("fRight");
         bLeft = hwMap.dcMotor.get("bLeft");
         bRight = hwMap.dcMotor.get("bRight");
-        lift = hwMap.dcMotor.get("lift");
+      //  lift = hwMap.dcMotor.get("lift");
+        arm = hwMap.dcMotor.get("arm");
 
 
-        jewelKnockDevice = hwMap.servo.get("jewelKnockDevice");
+       /* jewelKnockDevice = hwMap.servo.get("jewelKnockDevice");
         bLeftPincer = hwMap.servo.get("bLeftPincer");
         bRightPincer = hwMap.servo.get("bRightPincer");
         tLeftPincer = hwMap.servo.get("tLeftPincer");
         tRightPincer = hwMap.servo.get("tRightPincer");
+       */
+        clawServo = hwMap.servo.get("clawServo");
 
         fLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         fRight.setDirection(DcMotorSimple.Direction.FORWARD);
         bLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         bRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        lift.setDirection(DcMotorSimple.Direction.REVERSE);
+       // lift.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
-        bLeftPincer.setDirection(Servo.Direction.FORWARD);
+      /*  bLeftPincer.setDirection(Servo.Direction.FORWARD);
         bRightPincer.setDirection(Servo.Direction.REVERSE);
         tLeftPincer.setDirection(Servo.Direction.REVERSE);
         tRightPincer.setDirection(Servo.Direction.REVERSE);
-
+*/
         fLeft.setPower(0);
         fRight.setPower(0);
         bLeft.setPower(0);
         bRight.setPower(0);
-        lift.setPower(0);
+        //lift.setPower(0);
+        clawServo.setPosition(0);
 
 
         fLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     public void driveForword(double mult, double speed) {
@@ -91,7 +94,6 @@ with machanum wheels all 4 wheels need to be motorized
         fLeft.setTargetPosition((int) (1140 * mult));
         bRight.setTargetPosition((int) (1140 * mult));
         fRight.setTargetPosition((int) (1140 * mult));
-
 
 
         while (bLeft.isBusy()) ;
@@ -181,4 +183,7 @@ with machanum wheels all 4 wheels need to be motorized
         while (bLeft.isBusy()) ;
     }
 
-    }
+}
+
+
+
