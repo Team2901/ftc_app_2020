a8cde31cf19191ab7c486a559c9aab6ad0400680
zugc22484 <42450925+zugc22484@users.noreply.github.com>
2019-10-08 19:46:38 -0400

    added control for motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TriServoArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TriServoArmTest.java
@@ -2,46 +2,69 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp (name = "Tri Servo Arm Test")
 public class TriServoArmTest extends OpMode {
 
-    public Servo winchServo;
+    public DcMotor winchMotor;
+    public DcMotor wormMotor;
+
     public Servo rotatingServo;
     public Servo pinchServo;
 
     @Override
     public void init() {
-        winchServo = hardwareMap.servo.get("winchServo");
+        winchMotor = hardwareMap.dcMotor.get("winchMotor");
+        wormMotor = hardwareMap.dcMotor.get("wormMotor");
+
         rotatingServo = hardwareMap.servo.get("rotatingServo");
         pinchServo = hardwareMap.servo.get("pinchServo");
         rotatingServo.setPosition(0);
         pinchServo.setPosition(0);
+
     }
 
     @Override
     public void loop() {
-        double winchPosition = winchServo.getPosition();
         double leftStickY = -gamepad1.left_stick_y;
         if (leftStickY > 0.1) {
-            winchServo.setPosition(winchPosition + 0.01);
+            winchMotor.setPower(0.75);
         } else if (leftStickY < -0.1) {
-            winchServo.setPosition(winchPosition - 0.01);
+            winchMotor.setPower(-0.75);
+        } else{
+            winchMotor.setPower(0);
         }
+
+        double rightStickY = -gamepad1.right_stick_y;
+        if (rightStickY > 0.1) {
+            wormMotor.setPower(1);
+        } else if (rightStickY < -0.1) {
+            wormMotor.setPower(-1);
+        }else{
+            wormMotor.setPower(0);
+        }
+
         double positionRotatingServo = rotatingServo.getPosition();
-        if (gamepad1.a){
+        if (gamepad1.right_bumper){
             rotatingServo.setPosition(positionRotatingServo + 0.1);
-        } else if (gamepad1.b){
+        } else if (gamepad1.right_trigger > 0.1){
             rotatingServo.setPosition(positionRotatingServo - 0.1);
+        } else if(gamepad1.right_trigger == 1 && gamepad1.right_bumper == false){
+            rotatingServo.setPosition(positionRotatingServo);
         }
         double positionpinchServo = pinchServo.getPosition();
-        if (gamepad1.right_trigger > 0.1) {
+        if (gamepad1.left_bumper) {
             pinchServo.setPosition(positionpinchServo + 0.1);
         }else if (gamepad1.left_trigger > 0.1){
             pinchServo.setPosition((positionpinchServo - 0.1));
+        }else if(gamepad1.left_trigger == 1 && gamepad1.left_bumper == false) {
+            pinchServo.setPosition(positionRotatingServo);
         }
-        telemetry.addData("winchServo", winchPosition);
+        telemetry.addData("winchMotor", winchMotor.getCurrentPosition());
+        telemetry.addData("wormMotor", wormMotor.getCurrentPosition());
+
         telemetry.addData("rotatingServo", positionRotatingServo);
         telemetry.addData("pinchServo", positionpinchServo);
         telemetry.update();
