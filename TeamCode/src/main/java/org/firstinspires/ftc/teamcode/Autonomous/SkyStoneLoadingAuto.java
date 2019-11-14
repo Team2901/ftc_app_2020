06330ea3ce209006936d8ca6a5b51df446742bbf
Nicole C Allen <allenn23825@student.cghsnc.org>
2019-11-14 16:44:17 -0500

    Making autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SkyStoneLoadingAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SkyStoneLoadingAuto.java
@@ -3,9 +3,31 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.NewProgrammers.TestingTensorFlowWebcam;
+
 @Autonomous(name = "SkyStoneLoadingAuto")
 
 public class SkyStoneLoadingAuto extends LinearOpMode {
+
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_BUTTER = "Stone";
+    private static final String LABEL_SKY_BUTTER = "Skystone";
+    private int skyStonePosition;
+    private double skyStoneGridLocation;
+
+    private static final String VUFORIA_KEY ="AYhwTMH/////AAABmR7oFvU9lEJTryl5O3jDSusAPmWSAx5CHlcB/" +
+            "IUoT+t7S1pJqTo7n3OwM4f2vVULA0T1uZVl9i61kWldhVqxK2+kyBNI4Uld8cYgHaNIQFsL/NsyBrb3Zl+1ZFBR" +
+            "tpI5BjPnJkivkDsGU0rAFd+vPkyZt0p3/Uz+50eEwMZrZh499IsfooWkGX1wobjOFeA7DYQU+5ulhc1Rdp4mqjj" +
+            "uKrS24Eop0MKJ+PwvNJhnN4LqIWQSfSABmcw9ogaeEsCzJdowrpXAcSo9d+ykJFZuB92iKN16lC9dRG3PABt26o" +
+            "lSUCeXJrC4g6bEldHlmTc51nRpix6i1sGfvNuxlATzuRf5dtX/YlQm2WvvG9TilHbz";
+
+    private VuforiaLocalizer vuforia;
+
+    private TFObjectDetector tfod;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -21,6 +43,33 @@ public class SkyStoneLoadingAuto extends LinearOpMode {
             Move to waffle, place block
          7.) Place Capstone on block
          */
+        // Step 1 Find Skystone
+
+        initVuforia();
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod();
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+        }
+
+        if (tfod != null) {
+            tfod.activate();
+        } else {
+            telemetry.addData("We Regret to Inform You", "Tensor flow has failed to initialize");
+        }
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
+        waitForStart();
+
+        skyStonePosition = findSkyStone();
+
+        skyStoneGridLocation = convertPositionToGridOffset(skyStonePosition);
+
+
+        //Step 2 Get Skystone
+
+
 
         /*
         1.) Move waffle to the middle position
@@ -39,5 +88,46 @@ public class SkyStoneLoadingAuto extends LinearOpMode {
         10.) Go to step 3
          */
 
+    }
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.8;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_BUTTER, LABEL_SKY_BUTTER);
+    }
+
+    private int findSkyStone() {
+        return 1;
+    }
+
+    private double convertPositionToGridOffset (int position){
+        return 54.4;
+    }
+
+    private void moveForward (double inches){
+        telemetry.addData("Moved to a position in front of oneself", inches);
     }
 }
