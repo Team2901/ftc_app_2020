edeac09a23bf0a8a4ba19524357bc60353bacb93
Margaret Toebes <mtoebes@republicwireless.com>
2019-12-14 17:40:04 +0000

    add builderskystone hardware and teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/BuilderSkystoneHardware.java
@@ -0,0 +1,168 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.Utility.AngleUtilities;
+
+public class BuilderSkystoneHardware {
+
+    public final static double WHEEL_SERVO_GEAR_RATIO = .3;
+    public final static double WIDTH_OF_ROBOT = 13.5;
+    public final static double LENGTH_OF_ROBOT = 13.5;
+    //This is the angle Phi that we defined in the math done before this
+    public final static double TURN_ANGLE = Math.atan(WIDTH_OF_ROBOT/ LENGTH_OF_ROBOT);
+    public final static int SERVO_MAX_ANGLE = 2727;
+    public final static int SERVO_MIN_ANGLE = 0;
+    public final static double FRONT_LEFT_OFFSET = 0;//.11;
+    public final static double FRONT_RIGHT_OFFSET = 0;//.13;
+    public final static double BACK_LEFT_OFFSET = 0;//.1;
+    public final static double BACK_RIGHT_OFFSET = 0;//.11;
+    public final static double WHEEL_MIN_ANGLE = 0;
+    public final static double WHEEL_MAX_ANGLE = SERVO_MAX_ANGLE * WHEEL_SERVO_GEAR_RATIO;
+
+    public class SwerveWheel {
+        public double targetAngle = 0;
+        public int modifier = 1;
+        public double offset = 0;
+
+        public SwerveWheel(double offset) {
+            this.offset = offset;
+        }
+
+        public void setTargetAndModifier(double targetAngle, int modifier) {
+            this.targetAngle = targetAngle;
+            this.modifier = modifier;
+        }
+
+        public double wheelAngleToServoPosition(double wheelAngle) {
+            double servoAngle = wheelAngleToServoAngle(wheelAngle);
+            return servoAngleToServoPosition(servoAngle);
+        }
+
+        public double wheelAngleToServoPosition() {
+            return wheelAngleToServoPosition(targetAngle);
+        }
+
+        public double wheelAngleToServoAngle(double wheelAngle) {
+            return wheelAngle / WHEEL_SERVO_GEAR_RATIO;
+        }
+
+        public double servoAngleToServoPosition(double servoAngle) {
+            return (servoAngle / SERVO_MAX_ANGLE) + offset;
+        }
+    }
+
+    public class SwerveWheels {
+        public SwerveWheel frontLeftMotor = new SwerveWheel(FRONT_LEFT_OFFSET);
+        public SwerveWheel frontRightMotor = new SwerveWheel(FRONT_RIGHT_OFFSET);
+        public SwerveWheel backLeftMotor = new SwerveWheel(BACK_LEFT_OFFSET);
+        public SwerveWheel backRightMotor = new SwerveWheel(BACK_RIGHT_OFFSET);
+    }
+
+    public HardwareMap hardwareMap;
+
+    //Made for a 4 wheel swerve drive system
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+
+    //Steering servo for their respective motor
+    public Servo servoFrontLeft;
+    public Servo servoFrontRight;
+    public Servo servoBackLeft;
+    public Servo servoBackRight;
+
+    public SwerveWheels swerveWheels = new SwerveWheels();
+
+    //Sensors and Things
+    public BNO055IMU imu;
+    public IntegratingGyroscope gyroscope;
+
+    public double offset = 0;
+
+    public void init(HardwareMap hwMap) {
+        hardwareMap = hwMap;
+
+        //Inititialize all Motors
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+
+        //Initialize all servos
+        servoFrontLeft = hardwareMap.servo.get("servoFrontLeft");
+        servoFrontRight = hardwareMap.servo.get("servoFrontRight");
+        servoBackLeft = hardwareMap.servo.get("servoBackLeft");
+        servoBackRight = hardwareMap.servo.get("servoBackRight");
+
+        // setting up the gyroscope
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        gyroscope = (IntegratingGyroscope) imu;
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+
+    public double getRawAngle() {
+        Orientation orientation = gyroscope.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return AngleUtilities.getNormalizedAngle(orientation.firstAngle);
+    }
+
+    public double getAngle() {
+        return AngleUtilities.getNormalizedAngle(getRawAngle() + offset);
+    }
+
+    public void setWheelMotorPower(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower) {
+        backRight.setPower(-backRightPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
+        frontLeft.setPower(-frontLeftPower);
+    }
+
+    public void setWheelServoPosition(double fLPos, double fRPos, double bLPos, double bRPos) {
+        servoFrontRight.setPosition(fRPos);
+        servoBackRight.setPosition(bRPos);
+        servoFrontLeft.setPosition(fLPos);
+        servoBackLeft.setPosition(bLPos);
+    }
+
+    public void setWheelMotorMode(DcMotor.RunMode runMode) {
+        backLeft.setMode(runMode);
+        backRight.setMode(runMode);
+        frontLeft.setMode(runMode);
+        frontRight.setMode(runMode);
+    }
+
+    public boolean wheelsAreBusy() {
+        return frontRight.isBusy() || frontLeft.isBusy() || backLeft.isBusy() || backRight.isBusy();
+
+    }
+
+    public void wait(int milliseconds, LinearOpMode opMode) {
+        ElapsedTime timer = new ElapsedTime();
+        while (opMode.opModeIsActive() && timer.milliseconds() < milliseconds) {
+
+        }
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BuilderSkystoneTeleOp.java
@@ -0,0 +1,143 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Hardware.BuilderSkystoneHardware;
+import org.firstinspires.ftc.teamcode.Hardware.SkystoneHardware;
+import org.firstinspires.ftc.teamcode.Utility.AngleUtilities;
+
+import static org.firstinspires.ftc.teamcode.Hardware.SkystoneHardware.WHEEL_MAX_ANGLE;
+import static org.firstinspires.ftc.teamcode.Hardware.SkystoneHardware.WHEEL_MIN_ANGLE;
+import static org.firstinspires.ftc.teamcode.Utility.AngleUtilities.getNormalizedAngle;
+
+@SuppressLint("DefaultLocale")
+@TeleOp(name = "BUILDER SKYSTONE TELEOP", group = "competition")
+public class BuilderSkystoneTeleOp extends OpMode {
+
+    public BuilderSkystoneHardware robot = new BuilderSkystoneHardware();
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+        swerveStraight(0, 0);
+    }
+
+    @Override
+    public void loop()  {
+        double joystickPositionX = gamepad1.left_stick_x;
+        double joystickPositionY = -gamepad1.left_stick_y;
+
+        if (Math.abs(gamepad1.right_stick_x) > .1) {
+            double power = getPower(gamepad1.right_stick_x, 0, gamepad1.left_bumper);
+            swerveTurn(power);
+        } else if (AngleUtilities.getRadius(joystickPositionX, joystickPositionY) > .2) {
+            double power = getPower(joystickPositionX, joystickPositionY, gamepad1.left_bumper);
+            double joyWheelAngle = joystickPositionToWheelAngle(joystickPositionX, joystickPositionY);
+            swerveStraight(joyWheelAngle, power);
+        } else {
+            robot.setWheelMotorPower(0,0,0,0);
+        }
+
+        telemetry.addData("FL", String.format("angle: %.2f, mod: %d, pos: %.2f",
+                robot.swerveWheels.frontLeftMotor.targetAngle, robot.swerveWheels.frontLeftMotor.modifier, robot.swerveWheels.frontLeftMotor.wheelAngleToServoPosition()));
+        telemetry.addData("FR", String.format("angle: %.2f, mod: %d, pos: %.2f",
+                robot.swerveWheels.frontRightMotor.targetAngle, robot.swerveWheels.frontRightMotor.modifier, robot.swerveWheels.frontRightMotor.wheelAngleToServoPosition()));
+        telemetry.addData("BL", String.format("angle: %.2f, mod: %d, pos: %.2f",
+                robot.swerveWheels.backLeftMotor.targetAngle, robot.swerveWheels.backLeftMotor.modifier, robot.swerveWheels.backLeftMotor.wheelAngleToServoPosition()));
+        telemetry.addData("BR", String.format("angle: %.2f, mod: %d, pos: %.2f",
+                robot.swerveWheels.backRightMotor.targetAngle, robot.swerveWheels.backRightMotor.modifier, robot.swerveWheels.backRightMotor.wheelAngleToServoPosition()));
+
+        telemetry.update();
+    }
+
+    public double joystickPositionToWheelAngle(double joystickPositionX, double joystickPositionY) {
+        double wheelAngleRad = Math.atan2(joystickPositionY, joystickPositionX);
+        double wheelAngle = AngleUtilities.radiansDegreesTranslation(wheelAngleRad) - 90;
+        return AngleUtilities.getPositiveNormalizedAngle(wheelAngle);
+    }
+
+    public double getPower(double x, double y, boolean pause) {
+        if (pause) {
+            return 0;
+        } else {
+            return AngleUtilities.getRadius(x,y);
+        }
+    }
+
+    public void swerveStraight(double joyWheelAngle, double power) {
+        swerveMove(joyWheelAngle, joyWheelAngle, joyWheelAngle, joyWheelAngle, power);
+    }
+
+    public void swerveTurn(double power) {
+
+        double fLAngle = joystickPositionToWheelAngle(-1, -1);
+        double fRAngle = joystickPositionToWheelAngle(-1, 1);
+        double bLAngle = joystickPositionToWheelAngle(1, -1);
+        double bRAngle = joystickPositionToWheelAngle(1, 1);
+
+        swerveMove(fLAngle, fRAngle, bLAngle, bRAngle, power);
+    }
+
+    public void swerveMove(double fLAngle, double fRAngle, double bLAngle, double bRAngle, double power) {
+
+        angleCheck(fLAngle, robot.swerveWheels.frontLeftMotor);
+        angleCheck(fRAngle, robot.swerveWheels.frontRightMotor);
+        angleCheck(bLAngle, robot.swerveWheels.backLeftMotor);
+        angleCheck(bRAngle, robot.swerveWheels.backRightMotor);
+
+        double servoPositionfL = robot.swerveWheels.frontLeftMotor.wheelAngleToServoPosition();
+        double servoPositionfR = robot.swerveWheels.frontRightMotor.wheelAngleToServoPosition();
+        double servoPositionbL = robot.swerveWheels.backLeftMotor.wheelAngleToServoPosition();
+        double servoPositionbR = robot.swerveWheels.backRightMotor.wheelAngleToServoPosition();
+
+        robot.setWheelServoPosition(servoPositionfL, servoPositionfR, servoPositionbL, servoPositionbR);
+
+        double frontLeftPower = robot.swerveWheels.frontLeftMotor.modifier * power;
+        double frontRightPower = robot.swerveWheels.frontRightMotor.modifier * power;
+        double backLeftPower = robot.swerveWheels.backLeftMotor.modifier * power;
+        double backRightPower = robot.swerveWheels.backRightMotor.modifier * power;
+
+        robot.setWheelMotorPower(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+    }
+
+    public void angleCheck(double goal, BuilderSkystoneHardware.SwerveWheel swerveWheel) {
+
+        double start = swerveWheel.targetAngle;
+
+        goal = getNormalizedAngle(goal);
+
+        double dAngleForward = getNormalizedAngle(goal - start);
+        double targetAngleForward = dAngleForward + start;
+        boolean forwardPossible = (WHEEL_MIN_ANGLE <= targetAngleForward && targetAngleForward <= WHEEL_MAX_ANGLE);
+
+        double dAngleBackward = getNormalizedAngle(dAngleForward + 180);
+        double targetAngleBackward = dAngleBackward + start;
+        boolean backwardPossible = (WHEEL_MIN_ANGLE <= targetAngleBackward && targetAngleBackward <= WHEEL_MAX_ANGLE);
+
+        boolean goForward;
+
+        if (forwardPossible && backwardPossible) {
+            goForward = (Math.abs(dAngleForward) < Math.abs(dAngleBackward));
+        } else {
+            goForward = forwardPossible;
+        }
+
+        double targetAngle;
+        int modifier;
+
+        if (goForward) {
+            targetAngle = targetAngleForward;
+            modifier = 1;
+
+        } else {
+            targetAngle = targetAngleBackward;
+            modifier = -1;
+        }
+
+        swerveWheel.setTargetAndModifier(targetAngle, modifier);
+    }
+}
+
