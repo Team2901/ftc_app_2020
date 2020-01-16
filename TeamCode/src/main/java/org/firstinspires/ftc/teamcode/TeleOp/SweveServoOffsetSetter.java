740af42a61cc7051ba9f6106720080e389a2cba9
Margaret Toebes <mtoebes@republicwireless.com>
2020-01-16 23:13:26 +0000

    rename hardware2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SweveServoOffsetSetter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SweveServoOffsetSetter.java
@@ -1,9 +1,7 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -15,7 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@TeleOp(name = "Servo Offset Setter", group = "TEST")
+@TeleOp(name = "Servo Swerve Offset Setter", group = "TEST")
 public class SweveServoOffsetSetter extends OpMode {
 
     String CONFIG_FILENAME = "servo_offset_config.txt";
@@ -31,90 +29,90 @@ public class SweveServoOffsetSetter extends OpMode {
     int servoIndex;
 
     ArrayList<Servo> servoArrayList = new ArrayList<>();
-    
-    List<Integer> offsets = new ArrayList<>();
+
+    List<Double> offsets = new ArrayList<>();
 
     @Override
     public void init() {
-
         impGamepad = new ImprovedGamepad(this.gamepad1, this.timer, "GP");
 
         robot.init(hardwareMap);
 
         try {
-            offsets = FileUtilities.readIntegerConfigFile(CONFIG_FILENAME);
+            offsets = FileUtilities.readDoubleConfigFile(CONFIG_FILENAME);
         } catch (IOException e) {
             offsets = new ArrayList<>();
             for (int i = 0; i < servoNames.length; i++) {
-                offsets.add(0);
+                offsets.add(0.0);
             }
         }
 
-        for (int i = 0; i < servoNames.length; i++) {
-            offsets.add(0);
-        }
-        for (int i = 0; i < servoNames.length; i++) {
+        for(int i = 0; i < servoNames.length; i++){
             String servoName = servoNames[i];
             Servo servo = hardwareMap.servo.get(servoName);
             servoArrayList.add(servo);
             servo.setPosition(offsets.get(i));
-            telemetry.addData("Servo" + i, servoArrayList.get(i));
+            telemetry.addData("Servo"+i,servoArrayList.get(i));
         }
 
         telemetry.update();
     }
 
+    @Override
     public void loop() {
-            impGamepad.update();
+        impGamepad.update();
 
-            if(this.impGamepad.dpad_right.isInitialPress()){
-                servoIndex++;
-                if(servoIndex >= servoArrayList.size()){
-                    servoIndex = 0;
-                }
-            } else if(this.impGamepad.dpad_left.isInitialPress()) {
-                servoIndex--;
-                if(servoIndex < 0){
-                    servoIndex = servoArrayList.size() - 1;
-                }
+        if(this.impGamepad.dpad_right.isInitialPress()){
+            servoIndex++;
+            if(servoIndex >= servoArrayList.size()){
+                servoIndex = 0;
             }
-
-            servoUnderTest = servoArrayList.get(servoIndex);
-
-            if(servoUnderTest != null) {
-                if(this.impGamepad.left_bumper.isInitialPress()){
-                    servoUnderTest.setPosition(servoUnderTest.getPosition()-0.1);
-                } else if(this.impGamepad.right_bumper.isInitialPress()){
-                    servoUnderTest.setPosition(servoUnderTest.getPosition()+0.1);
-                } else if(this.impGamepad.left_trigger.isInitialPress()){
-                    servoUnderTest.setPosition(servoUnderTest.getPosition()-0.01);
-                } else if(this.impGamepad.right_trigger.isInitialPress()) {
-                    servoUnderTest.setPosition(servoUnderTest.getPosition()+0.01);
-                }
-
-                telemetry.addData("Left bumper","-0.1");
-                telemetry.addData("Right bumper","+0.1");
-                telemetry.addData("Left trigger","-0.2");
-                telemetry.addData("Right trigger","+0.2");
-                telemetry.addData("Position", servoUnderTest.getPosition());
-                telemetry.addData("Servo name",servoNames[servoIndex]);
+        } else if(this.impGamepad.dpad_left.isInitialPress()) {
+            servoIndex--;
+            if(servoIndex < 0){
+                servoIndex = servoArrayList.size() - 1;
             }
-
-            telemetry.addData("D Pad Right/Left", "Increment/decrement servos");
-            telemetry.addData("Press start to save values", "");
-
-            telemetry.update();
         }
+
+        servoUnderTest = servoArrayList.get(servoIndex);
+
+        if(servoUnderTest != null){
+            if(this.impGamepad.left_bumper.isInitialPress()){
+                servoUnderTest.setPosition(servoUnderTest.getPosition()-0.1);
+            } else if(this.impGamepad.right_bumper.isInitialPress()){
+                servoUnderTest.setPosition(servoUnderTest.getPosition()+0.1);
+            } else if(this.impGamepad.left_trigger.isInitialPress()){
+                servoUnderTest.setPosition(servoUnderTest.getPosition()-0.01);
+            } else if(this.impGamepad.right_trigger.isInitialPress()) {
+                servoUnderTest.setPosition(servoUnderTest.getPosition()+0.01);
+            }
+
+            offsets.set(servoIndex, servoUnderTest.getPosition());
+
+            telemetry.addData("Left bumper","-0.1");
+            telemetry.addData("Right bumper","+0.1");
+            telemetry.addData("Left trigger","-0.01");
+            telemetry.addData("Right trigger","+0.01");
+            telemetry.addData("Position", servoUnderTest.getPosition());
+            telemetry.addData("Servo name",servoNames[servoIndex]);
+        }
+
+        telemetry.addData("D Pad Right/Left", "Increment/decrement servos");
+
+        telemetry.update();
+
+    }
 
     @Override
     public void stop() {
+
         super.stop();
 
-            try {
-                FileUtilities.writeConfigFile(CONFIG_FILENAME, offsets);
-                telemetry.addData("Success writing to file", "");
-            } catch (Exception e) {
-                throw new RuntimeException("Error writing to file", e);
-            }
+        try {
+            FileUtilities.writeConfigFile(CONFIG_FILENAME, offsets);
+            telemetry.addData("Success writing to file", "");
+        } catch (Exception e) {
+            throw new RuntimeException("Error writing to file", e);
         }
+    }
 }
