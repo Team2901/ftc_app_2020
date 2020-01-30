7ee29be653a59562a270605f1cf0af889e3f69eb
Gilligan <kearneyg20428@student.cghsnc.org>
2020-01-30 15:42:38 -0500

    Create PotentiometerTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PotentiometerTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
+public class PotentiometerTest extends OpMode {
+
+    AnalogInput potentiometer;
+    double maxOgVoltage;
+
+    @Override
+    public void init() {
+        potentiometer = hardwareMap.analogInput.get("potentiometer");
+        maxOgVoltage = potentiometer.getMaxVoltage();
+    }
+
+    @Override
+    public void loop() {
+
+        double angle = (potentiometer.getVoltage()/potentiometer.getMaxVoltage())*270;
+        double badAngle = (potentiometer.getVoltage()/maxOgVoltage);
+        telemetry.addData("get Voltage" , potentiometer.getVoltage());
+        telemetry.addData("get Max Voltage" , potentiometer.getMaxVoltage());
+        telemetry.addData("Angle" , angle);
+        telemetry.addData("Connection Info" , potentiometer.getConnectionInfo());
+        telemetry.update();
+
+
+    }
+}
