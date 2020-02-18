047906eda75ba3dd7a94a73905dd405639884aab
meyersw23562 <55461160+meyersw23562@users.noreply.github.com>
2020-02-18 15:10:02 -0500

    Made InchesToEncoderTester
    testing how many encoders in an inch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/InchesToEncoderTester.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class InchesToEncoderTester extends BaseSkyStoneAuto{
+    @Override
+    public void runOpMode() throws InterruptedException {
+            robot.init(hardwareMap);
+           robot.swerveStraight(0,0);
+        waitForStart();
+        robot.setWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.setWheelMotorMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        while (opModeIsActive())
+        {
+            robot.moveStraight(1, 1000);
+            //measure inches traveled
+            //divide by encorders
+            // equal encoders to inche
+
+        }
+    }
+}
