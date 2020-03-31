7b5a151a3d1235e237b724fa584569e19ee85e9b
Margaret Toebes <mtoebes@republicwireless.com>
2020-03-31 20:20:03 +0000

    Create Dominator auto classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DominatorTankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DominatorTankDrive.java
@@ -1,4 +1,6 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -8,21 +10,15 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
-import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 /**
  * Created with Team 6183's Duckinator 3000
  */
 
-@Autonomous(name = "DuckinatorAuto", group = "DuckSquad")
-public class Dominator extends LinearOpMode {
-    private DcMotor fl;
-    private DcMotor fr;
-    private DcMotor bl;
-    private DcMotor br;
-    //holonomic encoder counts are slightly innacurate and need to be tested due to different amounts of force and friction on the wheels depending on what you get
-//please adjust personally to each program, we have accounted for slight slippage but just please make sure
+@Autonomous(name = "DominatorTankDrive", group = "DuckSquad")
+public class DominatorTankDrive extends LinearOpMode {
+    private DcMotor leftWheel;
+    private DcMotor rightWheel;
     private int globalAngle;
     BNO055IMU imu;
     Orientation lastAngles = new Orientation();
@@ -41,45 +37,40 @@ public class Dominator extends LinearOpMode {
             sleep(50);
             idle();
         }
-        fl = hardwareMap.dcMotor.get("fl");
-        fr = hardwareMap.dcMotor.get("fr");
-        bl = hardwareMap.dcMotor.get("bl");
-        br = hardwareMap.dcMotor.get("br");
+        leftWheel = hardwareMap.dcMotor.get("leftWheel");
+        rightWheel = hardwareMap.dcMotor.get("rightWheel");
+        rightWheel.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
         if (opModeIsActive()){
-            goForward(6351);
-            rotate(87);
-            goForward(5964);
+            goForward(3071);
+            rotate(-89);
+            goForward(8531);
+            rotate(90);
+            goForward(1007);
+            rotate(179);
+            goForward(1988);
+            rotate(-89);
+            goForward(4203);
 
         }
     }
     public void motorReset() {
-        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
     public void powerBusy() {
-        fl.setPower(0.5);
-        fr.setPower(0.5);
-        bl.setPower(0.5);
-        br.setPower(0.5);
-        while ((fl.isBusy() && fr.isBusy())&&(bl.isBusy() && br.isBusy())){}
-        fl.setPower(0);
-        fr.setPower(0);
-        bl.setPower(0);
-        br.setPower(0);
+        leftWheel.setPower(0.5);
+        rightWheel.setPower(0.5);
+        while ((rightWheel.isBusy() && leftWheel.isBusy())){}
+        leftWheel.setPower(0);
+        rightWheel.setPower(0);
     }
     public void goForward(int gofront){
         motorReset();
-        fl.setTargetPosition((int)Math.round(1.0*gofront));
-        fr.setTargetPosition((int)Math.round(-1.0*gofront));
-        bl.setTargetPosition((int)Math.round(1.0*gofront));
-        br.setTargetPosition((int)Math.round(1.0*gofront ));
+        rightWheel.setTargetPosition(gofront);
+        leftWheel.setTargetPosition(gofront);
         powerBusy();
     }
     private void resetAngle() {
@@ -98,35 +89,27 @@ public class Dominator extends LinearOpMode {
         return globalAngle;
     }
     private void rotate(int degrees) {
-        double flp, frp, blp, brp;
+        double leftPower, rightPower;
         resetAngle();
         if (degrees < 0) {   // turn right.
-            flp = 0.5;
-            frp = 0.5;
-            blp = 0.5;
-            brp = 0.5;
+            leftPower = 0.5;
+            rightPower = -0.5;
         }
         else if (degrees > 0) {   // turn left.
-            flp = -0.5;
-            frp = -0.5;
-            blp = -0.5;
-            brp = -0.5;
+            leftPower = -0.5;
+            rightPower = 0.5;
         }
         else return;
-        fl.setPower(flp);
-        fr.setPower(frp);
-        bl.setPower(blp);
-        br.setPower(brp);
+        leftWheel.setPower(leftPower);
+        rightWheel.setPower(rightPower);
         if (degrees < 0) {//right
             while (opModeIsActive() && getAngle() == 0) {}
             while (opModeIsActive() && getAngle() > degrees) {}
         } else {//left
             while (opModeIsActive() && getAngle() < degrees) {}
         }
-        fl.setPower(0);
-        fr.setPower(0);
-        bl.setPower(0);
-        br.setPower(0);
+        rightWheel.setPower(0);
+        leftWheel.setPower(0);
         sleep(1000);
         resetAngle();
     }
