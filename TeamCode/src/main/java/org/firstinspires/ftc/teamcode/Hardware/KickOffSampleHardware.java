bf758b7d67b730f33f672bb30d29482656f73a3b
meyersw23562 <55461160+meyersw23562@users.noreply.github.com>
2020-09-01 16:45:42 -0400

    Create KickOffSampleHardware.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/KickOffSampleHardware.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class KickOffSampleHardware {
+    public void init(HardwareMap hardwareMap){
+        
+    }
+}
