62df379977346adcfbf4fbc967b24a2f86c37701
NiknutMusic <54417256+NiknutMusic@users.noreply.github.com>
2020-09-01 17:25:33 -0400

    Added encoder stuff to hardware class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/KickOffSampleAutonomous.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+public class KickOffSampleAutonomous {
+    // Find wheel diameter
+    // gear ratio
+    // ticks per revolution
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/KickOffSampleHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/KickOffSampleHardware.java
@@ -4,6 +4,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class KickOffSampleHardware {
+    
+    public static final double TICKS_PER_MOTOR_REV = 1140;
+    public static final double DRIVE_GEAR_RATIO = 1;
+    public static final double TICKS_PER_DRIVE_REV = TICKS_PER_MOTOR_REV * DRIVE_GEAR_RATIO;
+    public static final double WHEEL_CIRCUMFERENCE = 4 * Math.PI;
+    public static final double TICKS_PER_INCH = TICKS_PER_DRIVE_REV / WHEEL_CIRCUMFERENCE;
 
     //Define the left and right motors as class variables
     public DcMotor leftDrive;
