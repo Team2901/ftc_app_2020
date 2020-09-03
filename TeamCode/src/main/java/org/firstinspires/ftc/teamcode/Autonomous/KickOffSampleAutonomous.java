df863870167790eab82b086bd9a2d82b14af2841
NiknutMusic <54417256+NiknutMusic@users.noreply.github.com>
2020-09-03 17:15:49 -0400

    Brought in the hardware to the autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/KickOffSampleAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/KickOffSampleAutonomous.java
@@ -3,10 +3,14 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Hardware.KickOffSampleHardware;
+
 @Autonomous (name = "Kick-off Sample Autonomous")
 public class KickOffSampleAutonomous extends LinearOpMode {
+    public KickOffSampleHardware robot = new KickOffSampleHardware();
     @Override
     public void runOpMode() throws InterruptedException {
-
+        robot.init(this.hardwareMap);
+        waitForStart();
     }
 }
