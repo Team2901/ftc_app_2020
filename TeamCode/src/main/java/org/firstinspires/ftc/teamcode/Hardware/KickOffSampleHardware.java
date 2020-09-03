fdc63dd6255896849b5c7535fc23641e5f4da1af
NiknutMusic <54417256+NiknutMusic@users.noreply.github.com>
2020-09-03 15:28:59 -0400

    Added comments to the encoder part of the hardware class for homework.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/KickOffSampleHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/KickOffSampleHardware.java
@@ -4,11 +4,27 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class KickOffSampleHardware {
-    
+
+    /*
+    This is the math that is used for encoders. The ticks per motor rev is
+    how many encoder ticks it takes for the motor to turn once
+     */
     public static final double TICKS_PER_MOTOR_REV = 1140;
+    //This is the ratio of the gears.
     public static final double DRIVE_GEAR_RATIO = 1;
+    /*
+    To find the number of encoder ticks it takes to make the wheel go around once,
+    you need to multiply the gear ratio by the motor tick count
+     */
     public static final double TICKS_PER_DRIVE_REV = TICKS_PER_MOTOR_REV * DRIVE_GEAR_RATIO;
+    /*
+    To find the wheel circumference, you need to multiply the wheel diameter by pi.
+     */
     public static final double WHEEL_CIRCUMFERENCE = 4 * Math.PI;
+    /*
+    This last one calculates how many encoder ticks it takes to go one inch.
+    It divides the ticks per wheel rotation by the wheel circumference.
+     */
     public static final double TICKS_PER_INCH = TICKS_PER_DRIVE_REV / WHEEL_CIRCUMFERENCE;
 
     //Define the left and right motors as class variables
