4718101dac1527553479334c240b3418d1ce7d0e
Margaret Toebes <margaret.toebes@gmail.com>
2020-09-05 10:56:18 -0400

    Update formating and comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/KickOffSampleAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/KickOffSampleAutonomous.java
@@ -6,42 +6,47 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Hardware.KickOffSampleHardware;
 
-@Autonomous (name = "Kick-off Sample Autonomous")
+@Autonomous(name = "Kick-off Sample Autonomous")
 public class KickOffSampleAutonomous extends LinearOpMode {
     public KickOffSampleHardware robot = new KickOffSampleHardware();
+
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(this.hardwareMap);
         waitForStart();
     }
-    public void moveInches (double inches){
-        //calculating target position
+
+    public void moveInches(double inches) {
+        // Calculate the target position
         int ticks = (int) (inches * robot.TICKS_PER_INCH);
 
-        //setting our target position
+        // Set the target position for each motor
         robot.leftDrive.setTargetPosition(robot.leftDrive.getCurrentPosition() + ticks);
         robot.rightDrive.setTargetPosition(robot.rightDrive.getCurrentPosition() + ticks);
 
-        //setting our motors to run to our target position
+        // Set the motors to run to a target position
         robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        //starting motors
+        // Start the motors (at half speed)
         robot.leftDrive.setPower(.5);
         robot.rightDrive.setPower(.5);
 
-        while(opModeIsActive() && (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())){
+        // Display the motors current positions while they are moving
+        // Note: Always check opModeIsActive() when using while loops
+        while (opModeIsActive() &&
+                (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
             telemetry.addData("Current Left Position", robot.leftDrive.getCurrentPosition());
             telemetry.addData("Current Right Position", robot.rightDrive.getCurrentPosition());
 
             telemetry.update();
         }
 
-        //stop motors
+        // Stop the motors
         robot.leftDrive.setPower(0);
         robot.rightDrive.setPower(0);
 
-        //resetting motors to run using encoders
+        // Reset the motors to run using encoders
         robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
